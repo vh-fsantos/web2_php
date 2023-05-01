f3a3a7e9b06da8e6bf16f9009569a0952a8a0458
Vitor Hugo Foscarini dos Santos <vitor-hugo962@hotmail.com>
2023-05-01 13:02:04 -0300

    create header and footer, form register user

--- users/register-user.php
+++ users/register-user.php
@@ -1,11 +1,10 @@
-<html lang="pt-br">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de Usuário</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  </head>
-  <body>
+<?php 
+$page_title = "Questionários";
+
+include_once("../common/facade.php");
+include_once("../common/header.php");
+
+?>
     <div class="container mt-5">
       <div class="row justify-content-center">
         <div class="col-md-6">
@@ -36,6 +35,5 @@
         </div>
       </div>
     </div>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>
+<?php include_once("../common/footer.php"); ?>
+    
