e357c766cc32b189fca8a824f21b46eeeed1e3d8
Pietro Belluno <pietrop@outlook.com>
2023-06-23 02:10:55 -0300

    grafico dashboard

--- dashboard/list.php
+++ dashboard/list.php
@@ -6,7 +6,7 @@ include_once("../common/facade.php");
 include_once("../common/header.php");
 
 $quizDao = $factory->getQuizDao();
-$quizzes = $quizDao->findAll(null, null , '');
+$quizzes = $quizDao->findAll(null, null, '');
 
 $offerDao = $factory->getOfferDao();
 $offers = $offerDao->findAll();
@@ -14,43 +14,96 @@ $offers = $offerDao->findAll();
 $submissionDao = $factory->getSubmissionDao();
 $submissions = $submissionDao->findAll();
 
-echo "<section class='container mt-4'>";
-echo "<div class='table-responsive'>";
-echo "<table class='table table-hover table-bordered'>";
-echo "<thead class='thead-light'>";
-echo "<tr>";
-echo "<th>Questionário</th>";
-echo "<th>Ofertas</th>";
-echo "<th>Submissões</th>";
-echo "</tr>";
-echo "</thead>";
-echo "<tbody>";
+// Prepare data for chart
+$chartLabels = array();
+$chartDataOffers = array();
+$chartDataSubmissions = array();
 
-foreach ($quizzes as &$quiz) {
+foreach ($quizzes as $quiz) {
     $quizId = $quiz->getId();
-	echo "<tr>";
-	echo "<td>{$quiz->getName()}</td>";
-    $count = 0;
-    foreach ($offers as &$offer){
-        if ($offer->getQuiz()->getId() == $quizId){
-            $count++;
+    $chartLabels[] = $quiz->getName();
+
+    $offerCount = 0;
+    foreach ($offers as $offer) {
+        if ($offer->getQuiz()->getId() == $quizId) {
+            $offerCount++;
         }
     }
-	echo "<td>$count</td>";
-    $count = 0;
-    foreach ($submissions as &$submission){
-        if ($submission->getOffer()->getQuiz()->getId() == $quizId){
-            $count++;
+    $chartDataOffers[] = $offerCount;
+
+    $submissionCount = 0;
+    foreach ($submissions as $submission) {
+        if ($submission->getOffer()->getQuiz()->getId() == $quizId) {
+            $submissionCount++;
         }
     }
-	echo "<td>$count</td>";
-    echo "</tr>";
+    $chartDataSubmissions[] = $submissionCount;
 }
 
-echo "</tbody>";
-echo "</table>";
-echo "</div>";
-echo "</section>";
+?>
 
+<section class="container mt-4">
+    <div class="table-responsive">
+        <table class="table table-hover table-bordered">
+            <thead class="thead-light">
+                <tr>
+                    <th>Questionário</th>
+                    <th>Ofertas</th>
+                    <th>Submissões</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($quizzes as $index => $quiz) { ?>
+                    <tr>
+                        <td><?php echo $quiz->getName(); ?></td>
+                        <td><?php echo $chartDataOffers[$index]; ?></td>
+                        <td><?php echo $chartDataSubmissions[$index]; ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</section>
+
+<div class="container mt-4">
+    <canvas id="chart"></canvas>
+</div>
+
+<?php 
 include_once("../common/footer.php"); 
 ?>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    // Create a bar chart
+    var ctx = document.getElementById('chart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: <?php echo json_encode($chartLabels); ?>,
+            datasets: [
+                {
+                    label: 'Ofertas',
+                    data: <?php echo json_encode($chartDataOffers); ?>,
+                    backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Submissões',
+                    data: <?php echo json_encode($chartDataSubmissions); ?>,
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
